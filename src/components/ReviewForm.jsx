3eaccf358559354eb9a86fb574fa12e15196d607
vieiragah <107413566+vieiragah@users.noreply.github.com>
2023-02-15 18:05:21 -0300

    finalização

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,10 +1,61 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import './ReviewForm.css'
 
 const ReviewForm = () => {
   return (
-    <div>
-        <h2>
-            Review Form
-        </h2>
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input 
+            type="radio" 
+            value='unsatisfied'
+            name="review"
+            required
+          />
+          <BsFillEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>  
+          <label className="radio-container">
+          <input 
+            type="radio" 
+            value='neutral'
+            name="review"
+            required
+          />
+          <BsFillEmojiNeutralFill />
+          <p>Poderia ser melhor</p>
+         </label> 
+          <label className="radio-container">
+          <input 
+            type="radio" 
+            value='satisfied'
+            name="review"
+            required
+          />
+          <BsFillEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+          <label className="radio-container">
+          <input 
+            type="radio" 
+            value='Very_satisfied'
+            name="review"
+            required
+          />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito satisfeito</p>
+        </label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="comment">Comentário:</label>
+        <textarea name="comment" id="comment" placeholder="Conte como foi a sua experiência com o produto" required></textarea>
+      </div>
     </div>
   )
 }
--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,8 +1,23 @@
-
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+import './Thanks.css'
 const Thanks = () => {
   return (
-    <div>
-        <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>A sua opnião é muito importante, em breve você receberá um cupom de 10% de desconto para a sua próxima compra.</p>
+      <p>Para concluir a avaliação clique no botão de "Enviar" abaixo.</p>
+      <h3>Aqui está o resumo da sua avaliação:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto:</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+      </p>
     </div>
   )
 }
